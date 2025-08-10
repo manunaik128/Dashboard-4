5ed32f1a97b88fa7faf42928312411c4dd2ed023
manunaik128 <manumn2005@gmail.com>
2025-08-10 13:51:01 +0530

    second commit

--- Dashboard-2/src/componant/Content1/Content1.jsx
+++ Dashboard-2/src/componant/Content1/Content1.jsx
@@ -15,6 +15,8 @@ import Gallery from '../../assets/gallery.png'
 import Video from '../../assets/video.png'
 import Music from '../../assets/music.png'
 import Docs from '../../assets/docs.png'
+import Folder from '../../assets/fold.png'
+import More from '../../assets/more.png'
 
 
 const Content1 = () => {
@@ -56,11 +58,11 @@ const Content1 = () => {
           <div className="search">
             <div className="text">
               <img src={Search} alt="" />
-                   <input type="text" placeholder='Search Something' />
-                      </div>
-                      <div className="btn">
-                           <button> <img src={New} alt="" />Create New</button>
-                      </div>
+              <input type="text" placeholder='Search Something' />
+            </div>
+            <div className="btn">
+              <button> <img src={New} alt="" />Create New</button>
+            </div>
 
           </div>
 
@@ -69,33 +71,140 @@ const Content1 = () => {
               <img src={File} alt="" /> <small>994 files</small>
             </div>
 
-             <div className="child">
+            <div className="child">
               <img src={Gallery} alt="" /> <small>2,328 files</small>
             </div>
-             <div className="child">
+            <div className="child">
               <img src={Video} alt="" /> <small>228 files</small>
             </div>
-             <div className="child">
+            <div className="child">
               <img src={Music} alt="" /> <small>424 files</small>
             </div>
-             <div className="child">
+            <div className="child">
               <img src={Docs} alt="" /> <small>824 files</small>
             </div>
           </div>
+            <div className="folder-name">
+              <h5>Folders</h5>
+              <h6>View All</h6>
+            </div>
+          <div className="midd">
+          
+          
 
-          <div className="folders">
-               Folder
-               <div className="backup-image">
-              <div className="backup-info"></div>
-               </div>
+            <div className="folders">
+
+              <div className="backup-image">
+                <div className="image1">
+                  <img src={Folder} alt="" />
+                </div>
+
+                <div className="image2">
+                  <img src={More} alt="" />
+                </div>
+              </div>
+
+
+
+              <div className="backup-info">
+                <h5>Behans Posts</h5>
+                <small>29 files</small>
+              </div>
+
+            </div>
+
+                    <div className="folders">
+
+              <div className="backup-image">
+                <div className="image1">
+                  <img src={Folder} alt="" />
+                </div>
+
+                <div className="image2">
+                  <img src={More} alt="" />
+                </div>
+              </div>
+
+
+
+              <div className="backup-info">
+                <h5>Behans Posts</h5>
+                <small>29 files</small>
+              </div>
+
+            </div>
+
+                    <div className="folders">
+
+              <div className="backup-image">
+                <div className="image1">
+                  <img src={Folder} alt="" />
+                </div>
+
+                <div className="image2">
+                  <img src={More} alt="" />
+                </div>
+              </div>
+
+
+
+              <div className="backup-info">
+                <h5>Behans Posts</h5>
+                <small>29 files</small>
+              </div>
+
+            </div>
+
+                    <div className="folders">
+
+              <div className="backup-image">
+                <div className="image1">
+                  <img src={Folder} alt="" />
+                </div>
+
+                <div className="image2">
+                  <img src={More} alt="" />
+                </div>
+              </div>
+
+
+
+              <div className="backup-info">
+                <h5>Behans Posts</h5>
+                <small>29 files</small>
+              </div>
+
+            </div>
+                    <div className="folders">
+
+              <div className="backup-image">
+                <div className="image1">
+                  <img src={Folder} alt="" />
+                </div>
+
+                <div className="image2">
+                  <img src={More} alt="" />
+                </div>
+              </div>
+
+
+
+              <div className="backup-info">
+                <h5>Behans Posts</h5>
+                <small>29 files</small>
+              </div>
+
+            </div>
           </div>
 
           <div className="files">
-         
+
           </div>
         </div>
 
-        <div className="right"></div>
+        <div className="right">
+
+        </div>
       </div>
 
     </>
