9759ec45df06c9e48b7f66c41a560bfd6bc2db0f
manunaik128 <manumn2005@gmail.com>
2025-08-20 20:07:04 +0530

    updated

--- Dashboard-2/src/componant/Content1/Content1.jsx
+++ Dashboard-2/src/componant/Content1/Content1.jsx
@@ -17,6 +17,12 @@ import Music from '../../assets/music.png'
 import Docs from '../../assets/docs.png'
 import Folder from '../../assets/fold.png'
 import More from '../../assets/more.png'
+import Top from '../../assets/top.png'
+import Notifi from '../../assets/notification.png'
+import Pro from '../../assets/profile.jpeg'
+import Down from '../../assets/down.png'
+import Storage from '../../assets/storage.png'
+import Import from '../../assets/import.png'
 
 
 const Content1 = () => {
@@ -84,13 +90,13 @@ const Content1 = () => {
               <img src={Docs} alt="" /> <small>824 files</small>
             </div>
           </div>
-            <div className="folder-name">
-              <h5>Folders</h5>
-              <h6>View All</h6>
-            </div>
+          <div className="folder-name">
+            <h5>Folders</h5>
+            <h6>View All</h6>
+          </div>
           <div className="midd">
-          
-          
+
+
 
             <div className="folders">
 
@@ -113,7 +119,7 @@ const Content1 = () => {
 
             </div>
 
-                    <div className="folders">
+            <div className="folders">
 
               <div className="backup-image">
                 <div className="image1">
@@ -128,13 +134,13 @@ const Content1 = () => {
 
 
               <div className="backup-info">
-                <h5>Behans Posts</h5>
-                <small>29 files</small>
+                <h5>Dribble Shots</h5>
+                <small>20 files</small>
               </div>
 
             </div>
 
-                    <div className="folders">
+            <div className="folders">
 
               <div className="backup-image">
                 <div className="image1">
@@ -149,13 +155,13 @@ const Content1 = () => {
 
 
               <div className="backup-info">
-                <h5>Behans Posts</h5>
-                <small>29 files</small>
+                <h5>Illustrations</h5>
+                <small>149 files</small>
               </div>
 
             </div>
 
-                    <div className="folders">
+            <div className="folders">
 
               <div className="backup-image">
                 <div className="image1">
@@ -170,12 +176,12 @@ const Content1 = () => {
 
 
               <div className="backup-info">
-                <h5>Behans Posts</h5>
-                <small>29 files</small>
+                <h5>Icons</h5>
+                <small>59 files</small>
               </div>
 
             </div>
-                    <div className="folders">
+            <div className="folders">
 
               <div className="backup-image">
                 <div className="image1">
@@ -190,8 +196,8 @@ const Content1 = () => {
 
 
               <div className="backup-info">
-                <h5>Behans Posts</h5>
-                <small>29 files</small>
+                <h5>Posters</h5>
+                <small>80 files</small>
               </div>
 
             </div>
@@ -199,10 +205,144 @@ const Content1 = () => {
 
           <div className="files">
 
+            <div className="name">
+              <p>Names <img src={Top} alt="" /></p>
+
+              <a href=""><img src={Video} alt="" /> Proposal.docx</a> <br />
+              <a href=""><img src={Video} alt="" /> Proposal.docx</a> <br />
+              <a href=""><img src={Video} alt="" /> Proposal.docx</a> <br />
+              <a href=""><img src={Video} alt="" /> Proposal.docx</a> <br />
+              <a href=""><img src={Video} alt="" /> Proposal.docx</a> <br />
+            </div>
+            <div className="size">
+              <p>Size <img src={Top} alt="" /> </p><br />
+
+              <a href="">2.9 MB</a>
+              <a href="">2.9 MB</a>
+              <a href="">2.9 MB</a>
+              <a href="">2.9 MB</a>
+            </div>
+            <div className="last-modified">
+              <p>Last-modified <img src={Top} alt="" /></p>
+            </div>
+            <div className="members">
+              <p>Members <img src={Top} alt="" /></p>
+            </div>
+
           </div>
         </div>
 
         <div className="right">
+
+          <div className="top">
+            <div className="notification">
+              <img src={Notifi} alt="" />
+            </div>
+            <div className="profile">
+              <img src={Pro} alt="" />
+              <div className="down">
+                <img src={Down} alt="" />
+              </div>
+            </div>
+          </div>
+          <h5>Storage</h5>
+
+          <div className="storage">
+
+
+            <img src={Storage} alt="" />
+          </div>
+
+          <div className="storage-items">
+
+            <div className="items">
+            <div className="details">
+                  <div className="st-image">
+                  <img src={File} alt="" />
+                  </div>
+                <div className="st-detail">
+                  <h5>Document</h5>
+                  <small>994 files</small>
+                </div>
+                </div>
+
+              <div className="data">
+                <h4>2.5GB</h4>
+              </div>
+              </div>
+
+              <div className="items">
+            <div className="details">
+                  <div className="st-image">
+                  <img src={File} alt="" />
+                  </div>
+                <div className="st-detail">
+                  <h5>Document</h5>
+                  <small>994 files</small>
+                </div>
+                </div>
+
+              <div className="data">
+                <h4>2.5GB</h4>
+              </div>
+              </div>
+
+              <div className="items">
+            <div className="details">
+                  <div className="st-image">
+                  <img src={File} alt="" />
+                  </div>
+                <div className="st-detail">
+                  <h5>Document</h5>
+                  <small>994 files</small>
+                </div>
+                </div>
+
+              <div className="data">
+                <h4>2.5GB</h4>
+              </div>
+              </div>
+
+              <div className="items">
+            <div className="details">
+                  <div className="st-image">
+                  <img src={File} alt="" />
+                  </div>
+                <div className="st-detail">
+                  <h5>Document</h5>
+                  <small>994 files</small>
+                </div>
+                </div>
+
+              <div className="data">
+                <h4>2.5GB</h4>
+              </div>
+              </div>
+
+              <div className="items">
+            <div className="details">
+                  <div className="st-image">
+                  <img src={File} alt="" />
+                  </div>
+                <div className="st-detail">
+                  <h5>Document</h5>
+                  <small>994 files</small>
+                </div>
+                </div>
+
+              <div className="data">
+                <h4>2.5GB</h4>
+              </div>
+              </div>
+            
+          </div>
+          <div className="import">
+            <div className="import-file">
+            <img src={Import} alt="" />
+            <h3>Import Files</h3>
+            </div>
+          </div>
+
 
         </div>
       </div>
